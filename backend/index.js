4672f5cfc6676597e3537e4ad0375d05947468e4
doodger <jcrondyt@gmail.com>
2020-10-15 15:39:58 -0400

    more backend work, implementation of axios and cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json() );
 app.use(bodyParser.json());
 app.use('/api', routes);
--- src/components/pinBoard.component.js
+++ src/components/pinBoard.component.js
@@ -2,14 +2,29 @@
 import React, { useRef, useState, useEffect} from 'react';
 import { Pin } from './pin.component'
 import { PinCloseUp } from './pinCloseUp.component'
-
+import axios from 'axios'
 
 
 export default function PinBoard(props) {
     const [pinSelected, setPinSelected] = useState(null);
+    const [collectionOfPins, setCollectionOfPins] = useState([]);
+
+     useEffect( () => {
+        axios.get('http://localhost:5000/api/pins')
+        .then( response => { console.log("first",response.data.pins);
+                console.log("here is ", response.data.pins); 
+                setCollectionOfPins(response.data.pins);
+                console.log("turned into ", collectionOfPins)
+                
+            }).catch( (error) => {
+                console.log(error);
+            });
+    
+    }, []);
+
     var listOfPins = []
     listOfPins.push( { imgSource : "/fox.jpg" , alt : "forest dog"} )
-    listOfPins.push( { imgSource : "/foxPortraitBig.jpg" , alt : "forest dog"} )
+    /**listOfPins.push( { imgSource : "/foxPortraitBig.jpg" , alt : "forest dog"} )
     listOfPins.push( { imgSource : "/foxSnowBig.jpg" , alt : "forest dog"} )
     listOfPins.push( { imgSource : "/foxPortraitBig.jpg" , alt : "forest dog"} )
     listOfPins.push( { imgSource : "/fox.jpg" , alt : "forest dog"} )
@@ -32,8 +47,16 @@ export default function PinBoard(props) {
     listOfPins.push( { imgSource : "/foxPortraitBig.jpg" , alt : "forest dog"} )
     listOfPins.push( { imgSource : "/foxSnowBig.jpg" , alt : "forest dog"} )
     listOfPins.push( { imgSource : "/foxPortraitBig.jpg" , alt : "forest dog"} )
+    **/
+   
+
+    //listOfPins.push( { imgSource : collectionOfPins[0].imgSource ,
+    // alt : collectionOfPins[0].alt} )
+
+    //listOfPins.push( {collectionOfPins{1}})
 
     function renderListOfPins(a){
+        console.log("and here ", collectionOfPins[0])
         return( a.map( pin => { return <Pin 
             imgSource = {pin.imgSource} 
             alt = {pin.alt} 
@@ -45,6 +68,7 @@ export default function PinBoard(props) {
 
 
 return( !pinSelected ? (
+  
     <div className = "board">
         <div className = "rowOfPins" > { renderListOfPins(listOfPins.slice(0,3)) }</div>
         <div className = "rowOfPins" > { renderListOfPins(listOfPins.slice(3,6)) }</div>
